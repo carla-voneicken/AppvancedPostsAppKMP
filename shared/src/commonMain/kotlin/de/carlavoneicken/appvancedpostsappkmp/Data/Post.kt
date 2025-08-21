2accf594e93924434e532eefb50d56c704f2cee8
carla-voneicken <carlavoneicken@googmail.com>
2025-08-21 15:33:13 +0200

    Moved data classes Post and User to Shared

--- shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/Data/Post.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/Data/Post.kt
@@ -1,5 +1,8 @@
 package de.carlavoneicken.appvancedpostsappkmp.Data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Post(
     val userId: Int,
     val id: Int,
--- shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/Data/User.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/Data/User.kt
@@ -1,5 +1,8 @@
 package de.carlavoneicken.appvancedpostsappkmp.Data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class User(
     val id: Int,
     val username: String
