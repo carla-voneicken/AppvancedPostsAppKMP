23c84ebfb8da659e341146f179a7c87e5d6bf6eb
carla-voneicken <carlavoneicken@googlemail.com>
2025-08-22 16:30:20 +0200

    Implemented HttpRequest functionality including safeCall function for error catching and first method in the PostRepository

--- shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/network/PostsRepository.kt
+++
@@ -1,5 +0,0 @@
-package de.carlavoneicken.appvancedpostsappkmp.business.network
-
-object PostsRepository {
-
-}
---
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/utils/safeCall.kt
@@ -0,0 +1,42 @@
+package de.carlavoneicken.appvancedpostsappkmp.business.utils
+
+import io.ktor.client.call.body
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.serialization.SerializationException
+
+/* catch exceptions thrown by Ktor/network/JSON using a try-catch-block
+-> these happen BEFORE we even get a response from the server, e.g. the request fails to reach the
+server or the JSON can't be deserialized */
+suspend fun <T> safeCall(
+    // block: the actual HTTP request or operation that could fail
+    block: suspend () -> HttpResponse,
+    // mapper: transforms the raw result of the block into the desired output type (e.g. List<Post>)
+    // it's generic so safeCall doesn't need to know in advance what the final type is, we supply it with the mapper
+    mapper: suspend (HttpResponse) -> T
+): Result<T, NetworkError> {
+    return try {
+        val response = block()  // run the HTTP request
+
+        /* check HTTP status codes returned by the server -> the request might succeed at the network
+        level, but the server responds with 401, ,500 etc. */
+        when (response.status.value) {
+            in 200..299 -> {
+                val body: T = mapper(response)
+                Result.Success(body)
+            }
+            401 -> Result.Failure(NetworkError.UNAUTHORIZED)
+            408 -> Result.Failure(NetworkError.REQUEST_TIMEOUT)
+            409 -> Result.Failure(NetworkError.CONFLICT)
+            413 -> Result.Failure(NetworkError.PAYLOAD_TOO_LARGE)
+            in 500..599 -> Result.Failure(NetworkError.SERVER_ERROR)
+            else -> Result.Failure(NetworkError.UNKNOWN)
+        }
+    } catch (e: UnresolvedAddressException) {
+        Result.Failure(NetworkError.NO_INTERNET)
+    } catch (e: SerializationException) {
+        Result.Failure(NetworkError.SERIALIZATION)
+    } catch (e: Exception) {
+        Result.Failure(NetworkError.UNKNOWN)
+    }
+}
