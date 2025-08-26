62ab58a205c835dfe5c8906f2f0e8363086933d4
carla-voneicken <carlavoneicken@googlemail.com>
2025-08-26 10:56:55 +0200

    Koin initialization

--- composeApp/src/androidMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/MainActivity.kt
@@ -3,23 +3,16 @@ package de.carlavoneicken.appvancedpostsappkmp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import de.carlavoneicken.appvancedpostsappkmp.di.initKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        InitKoinKt.initKoin()
+        initKoin()
 
         setContent {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
