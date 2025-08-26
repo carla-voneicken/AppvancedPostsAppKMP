bdba67fbcede4672140f5721a0ae856a55b6d3cd
carla-voneicken <carlavoneicken@googmail.com>
2025-08-26 11:00:31 +0200

    Implemented UseCases

---
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/usecases/GetPostByIdUsecase.kt
@@ -0,0 +1,14 @@
+package de.carlavoneicken.appvancedpostsappkmp.business.usecases
+
+import de.carlavoneicken.appvancedpostsappkmp.business.utils.NetworkError
+import de.carlavoneicken.appvancedpostsappkmp.business.utils.NetworkResult
+import de.carlavoneicken.appvancedpostsappkmp.data.Post
+import de.carlavoneicken.appvancedpostsappkmp.data.repositories.PostsRepository
+
+class GetPostByIdUsecase(private val postsRepository: PostsRepository) {
+
+    suspend operator fun invoke(id: Int): NetworkResult<Post, NetworkError> {
+        return postsRepository.getPostById(id)
+    }
+
+}
---
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/usecases/GetUserUsecase.kt
@@ -0,0 +1,13 @@
+package de.carlavoneicken.appvancedpostsappkmp.business.usecases
+
+import de.carlavoneicken.appvancedpostsappkmp.business.utils.NetworkError
+import de.carlavoneicken.appvancedpostsappkmp.business.utils.NetworkResult
+import de.carlavoneicken.appvancedpostsappkmp.data.User
+import de.carlavoneicken.appvancedpostsappkmp.data.repositories.UsersRepository
+
+class GetUserUsecase(private val usersRepository: UsersRepository) {
+
+    suspend operator fun invoke(): NetworkResult<List<User>, NetworkError> {
+        return usersRepository.getUsers()
+    }
+}
