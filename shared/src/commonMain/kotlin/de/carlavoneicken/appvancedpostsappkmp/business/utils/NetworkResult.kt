77f159431d76c191cf79abf6962f7f84ca52f2b7
carla-voneicken <carlavoneicken@googmail.com>
2025-08-28 14:26:13 +0200

    New explanatory comment

--- shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/utils/NetworkResult.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/utils/NetworkResult.kt
@@ -4,3 +4,10 @@ sealed interface NetworkResult<out D, out E: Error> {
     data class Success<out D>(val data: D): NetworkResult<D, Nothing>
     data class Failure<out E: Error>(val error: E): NetworkResult<Nothing, E>
 }
+
+/*
+val result: Result<Post, NetworkError> = repository.createPost(post)
+
+-> If Result.Success -> result.data is a Post
+-> If Result.Failure -> result.error is a NetworkError
+ */
