976bb6da9454509a04deb6370f1085e8a7218f63
carla-voneicken <carlavoneicken@googlemail.com>
2025-08-28 15:33:29 +0200

    Deleted unused imports

--- shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/network/HttpClientProvider.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/network/HttpClientProvider.kt
@@ -1,7 +1,6 @@
 package de.carlavoneicken.appvancedpostsappkmp.network
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
