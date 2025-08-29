3ba4c6f5dde646a096aed0a61d60bf99d121ff00
carla-voneicken <carlavoneicken@googlemail.com>
2025-08-29 14:25:30 +0200

    Created data types for Post and User in Swift so they conform to Identifiable (which Kotlin data types can't)

---
+++ iosApp/iosApp/Data/IdentifiableUser.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  iosApp
+//
+//  Created by Carla von Eicken on 29.08.25.
+//
+
+import Shared
+
+struct IdentifiableUser: Identifiable {
+    let id: Int
+    let username: String
+}
+
