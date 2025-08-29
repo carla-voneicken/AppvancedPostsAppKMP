86371f90dc9c881ed465ec85c1af2942b1d95b71
carla-voneicken <carlavoneicken@googlemail.com>
2025-08-29 14:24:56 +0200

    Made KMP Observable Viewmodel work in Swift

---
+++ iosApp/iosApp/KMPObservableViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  KMPObservableViewModel.swift
+//  iosApp
+//
+//  Created by Carla von Eicken on 29.08.25.
+//
+
+import KMPObservableViewModelCore
+import Shared
+
+extension Kmp_observableviewmodel_coreViewModel: ViewModel { }
