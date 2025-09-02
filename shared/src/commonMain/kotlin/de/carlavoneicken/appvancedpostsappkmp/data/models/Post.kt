70c58cb875707a3c8f5fc2f2bed5d63e9222e826
Carla von Eicken <carlavoneicken@googlemail.com>
2025-09-02 19:53:12 +0200

    Changed Post.kt back to a regular data class (not an entity)

--- shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/data/models/Post.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/data/models/Post.kt
@@ -1,11 +1,13 @@
 package de.carlavoneicken.appvancedpostsappkmp.data
 
+import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Post(
-    val userId: Int,
+    @PrimaryKey(autoGenerate = false)
     val id: Int,
+    val userId: Int,
     val title: String,
     val body: String
 )
