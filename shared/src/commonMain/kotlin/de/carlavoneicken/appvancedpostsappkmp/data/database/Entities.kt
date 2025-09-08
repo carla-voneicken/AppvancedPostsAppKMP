eadc5509f5b94010f2af7400ac8ac130a548de30
carla-voneicken <carlavoneicken@googlemail.com>
2025-09-08 21:07:12 +0200

    Simplified the Entities

--- shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/data/database/Entities.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/data/database/Entities.kt
@@ -1,9 +1,7 @@
 package de.carlavoneicken.appvancedpostsappkmp.data.database
 
 import androidx.room.Entity
-import androidx.room.Index
 import androidx.room.PrimaryKey
-import de.carlavoneicken.appvancedpostsappkmp.data.database.utils.SyncState
 
 @Entity(tableName = "users")
 data class UserEntity(
@@ -13,19 +11,18 @@ data class UserEntity(
 
 @Entity(
     tableName = "posts",
-    indices = [
-        // unique id required so that we can do an upsert on the existing database using the data from
-        // the API (which has the remoteId as primary key)
-        Index("remoteId", unique = true),
-        Index("userRemoteId")
-    ]
+//    indices = [
+//        // unique id required so that we can do an upsert on the existing database using the data from
+//        // the API (which has the remoteId as primary key)
+//        Index("remoteId", unique = true),
+//        Index("userRemoteId")
+//    ]
 )
 data class PostEntity(
-    @PrimaryKey(autoGenerate = true) val localId: Long = 0, // local primary key
-    val remoteId: Long?, // API id (null until server assigns)
-    val userRemoteId: Long,
+    @PrimaryKey val id: Long,
+    val userId: Long,
     val title: String,
     val body: String,
-    val syncState: SyncState = SyncState.SYNCED,
-    val updatedAt: Long
+//    val syncState: SyncState = SyncState.SYNCED,
+//    val updatedAt: Long
 )
