3e00ad1481e473e08bee51d5734d306b23358784
carla-voneicken <carlavoneicken@googlemail.com>
2025-09-09 15:50:15 +0200

    Refactored Usecases to work with the new repositories

--- shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/usecases/GetPostByUserIdUsecase.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/usecases/GetPostByUserIdUsecase.kt
@@ -1,14 +1,13 @@
 package de.carlavoneicken.appvancedpostsappkmp.business.usecases
 
-import de.carlavoneicken.appvancedpostsappkmp.business.utils.NetworkError
-import de.carlavoneicken.appvancedpostsappkmp.business.utils.NetworkResult
 import de.carlavoneicken.appvancedpostsappkmp.data.models.Post
 import de.carlavoneicken.appvancedpostsappkmp.data.repositories.PostsRepository
+import kotlinx.coroutines.flow.Flow
 
 class GetPostByUserIdUsecase(private val postsRepository: PostsRepository) {
 
-    suspend operator fun invoke(userId: Long): NetworkResult<List<Post>, NetworkError> {
-        return postsRepository.getPostsByUserId(userId)
+    suspend operator fun invoke(userId: Long): Flow<List<Post>> {
+        return postsRepository.observePostsByUserId(userId)
     }
 
 }
--- shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/usecases/GetUserUsecase.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/appvancedpostsappkmp/business/usecases/GetUserUsecase.kt
@@ -1,13 +1,12 @@
 package de.carlavoneicken.appvancedpostsappkmp.business.usecases
 
-import de.carlavoneicken.appvancedpostsappkmp.business.utils.NetworkError
-import de.carlavoneicken.appvancedpostsappkmp.business.utils.NetworkResult
 import de.carlavoneicken.appvancedpostsappkmp.data.models.User
 import de.carlavoneicken.appvancedpostsappkmp.data.repositories.UsersRepository
+import kotlinx.coroutines.flow.Flow
 
 class GetUserUsecase(private val usersRepository: UsersRepository) {
 
-    suspend operator fun invoke(): NetworkResult<List<User>, NetworkError> {
-        return usersRepository.getUsers()
+    suspend operator fun invoke(): Flow<List<User>> {
+        return usersRepository.observeUsers()
     }
 }
