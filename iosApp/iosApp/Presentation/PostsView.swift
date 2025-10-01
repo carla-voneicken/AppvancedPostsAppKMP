5e2ba586bee52a9895a1e24979648f7bd12a026a
carla-voneicken <carlavoneicken@googlemail.com>
2025-10-01 10:16:54 +0200

    Refactored the Views so they can use the Kotlin data class for Post and User and I don't need the IdentifiablePost and IdentifiableUser structs anymore

--- iosApp/iosApp/Presentation/PostsView.swift
+++ iosApp/iosApp/Presentation/PostsView.swift
@@ -12,16 +12,6 @@ import Shared
 struct PostsView: View {
     @StateViewModel var viewmodel: PostsViewModel
     
-    private var identifiablePosts: [IdentifiablePost] {
-        viewmodel.uiState.posts.map {
-            IdentifiablePost(
-                id: Int($0.id),
-                userId: Int($0.userId),
-                title: $0.title,
-                body: $0.body
-            )
-        }
-    }
     
     var body: some View {
         if viewmodel.uiState.isLoading {
@@ -31,7 +21,7 @@ struct PostsView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
         } else {
             VStack {
-                List(identifiablePosts) { post in
+                List(viewmodel.uiState.posts, id: \.id) { post in
                     NavigationLink(
                         destination: createPostDetailView(for: post)
                     ) {
@@ -61,7 +51,7 @@ struct PostsView: View {
     
     // Helper Methods
     @ViewBuilder
-    private func createPostDetailView(for post: IdentifiablePost) -> some View {
+    private func createPostDetailView(for post: Post) -> some View {
         PostDetailView(
             viewmodel: PostDetailViewModel(
                 postId: KotlinLong(value: Int64(post.id)),  // Convert Swift Int to KotlinInt
@@ -83,7 +73,7 @@ struct PostsView: View {
 
 
 struct PostRow: View {
-    let post: IdentifiablePost
+    let post: Post
     
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
